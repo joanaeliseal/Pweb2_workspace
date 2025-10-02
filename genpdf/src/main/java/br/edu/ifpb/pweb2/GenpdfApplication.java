97d6964d479e59c9f46f3020abd23e00e4a24409
Joana Elise <112410414+joanaeliseal@users.noreply.github.com>
2025-10-01 21:22:32 -0300

    Add files via upload

---
+++ genpdf/src/main/java/br/edu/ifpb/pweb2/GenpdfApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//import javax.swing.Spring;
+
+@SpringBootApplication
+public class GenpdfApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GenpdfApplication.class, args);
+    }
+}
